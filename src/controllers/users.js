404efbf9e0132d08a71ed50f952a452961464d0b
Eduardo Souza <edcampossouza@gmail.com>
2023-02-14 14:25:43 -0300

    feat: GET/ users/me

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,3 +1,26 @@
-export function userData(_, res) {
-  return res.status(200).send(res.locals?.user);
+import { db } from "../config/database.js";
+
+export async function userData(_, res) {
+  const { id: userId, name: userName } = res.locals.user;
+  try {
+    const visitCount = await db.query(
+      'SELECT SUM("visitCount") AS "count" FROM "url" WHERE "user" = $1',
+      [userId]
+    );
+    const urls = await db.query(
+      'SELECT "id", "shortUrl", "url", "visitCount" FROM "url" WHERE "user" = $1',
+      [userId]
+    );
+
+    const returnObj = {
+      id: userId,
+      name: userName,
+      vistCount: visitCount.rowCount > 0 ? Number(visitCount.rows[0].count) : 0,
+      shortenedUrls: urls.rows,
+    };
+    return res.status(200).send(returnObj);
+  } catch (error) {
+    console.log(error.message);
+    return res.sendStatus(500);
+  }
 }
